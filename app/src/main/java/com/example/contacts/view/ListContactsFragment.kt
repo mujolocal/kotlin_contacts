7b3c1367f97e3909061f02f78de52d9d36e16489
muammar johnson <muammarj@gmail.com>
2021-02-21 16:57:34 -0600

    first fragment added

---
+++ app/src/main/java/com/example/contacts/view/ListContactsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.contacts.view
+
+import androidx.fragment.app.Fragment
+import com.example.contacts.R
+
+class ListContactsFragment: Fragment(R.layout.fragment_list_contacts) {
+
+}
--- app/src/main/java/com/example/contacts/view/MainActivity.kt
+++ app/src/main/java/com/example/contacts/view/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.contacts.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
 import com.example.contacts.R
+import com.example.contacts.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.txtview.text = "the binding is complete"
+        if(savedInstanceState == null){
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add(binding.fragmentContainer.id,ListContactsFragment())
+            }
+        }
     }
 }
