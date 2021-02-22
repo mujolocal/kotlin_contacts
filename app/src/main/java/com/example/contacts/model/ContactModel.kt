984b29215f0021c01f0cf25776d1ab9aeaea323e
muammar johnson <muammarj@gmail.com>
2021-02-21 22:08:29 -0600

    created creation of model object

--- app/src/main/java/com/example/contacts/model/ContactModel.kt
+++ app/src/main/java/com/example/contacts/model/ContactModel.kt
@@ -3,10 +3,15 @@ package com.example.contacts.model
 import java.io.Serializable
 
 data class ContactModel(
-    val fName:String,
-    val lName:String,
+    val fName:String?,
+    val lName:String?,
     val ADDRESS: Address?,
-    val phone: List<String>?,
-    val email: List<String>?,
+    val phone: String?,
+    val email: String?,
 
-): Serializable
+    ): Serializable{
+    override fun toString(): String {
+        return "firstName: $fName, LastName: $lName, Phone: $phone "
+    }
+
+    }
--- app/src/main/java/com/example/contacts/view/CrudFragment.kt
+++ app/src/main/java/com/example/contacts/view/CrudFragment.kt
@@ -1,21 +1,42 @@
 package com.example.contacts.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.contacts.R
 import com.example.contacts.databinding.FragmentCrudBinding
+import com.example.contacts.model.Address
+import com.example.contacts.model.ContactModel
 
 class CrudFragment: Fragment(R.layout.fragment_crud) {
+    private val TAG = "CrudFragment"
     lateinit var binding: FragmentCrudBinding
+    lateinit var contactModel: ContactModel
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentCrudBinding.inflate(layoutInflater)
+        binding.commitBtn.setOnClickListener {
+            contactModel = ContactModel(
+                binding.crudFnameIv.text.toString(),
+                binding.crudLnameIv.text.toString(),
+                Address(
+                    binding.crudStreetIv.text.toString(),
+                    binding.crudCityIv.text.toString(),
+                    binding.crudStateIv.text.toString(),
+                    binding.crudZipIv.text.toString()
+                ),
+                binding.crudPhoneIv.text.toString(),
+                binding.crudEmailIv.text.toString()
+            )
+            Log.d(TAG, "onCreateView: $contactModel ")
+        }
+
 
         return binding.root
     }
